2b09307e3179bc9b66d4ef7f42bbc6361816d025
Krishnan Kumar <kkrish@google.com>
2022-09-27 21:24:56 +0530

    mimetype admin static commit

--- sipa/settings.py
+++ sipa/settings.py
@@ -142,13 +142,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # VENV_PATH = os.path.dirname(BASE_DIR)
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static/'),
+# )
 
 # STATICFILES_FINDERS = (
 #     'django.contrib.staticfiles.finders.FileSystemFinder',
