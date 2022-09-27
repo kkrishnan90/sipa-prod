7595fc2c6e8c0eaa3bdb0fb86202db99fbbda130
Krishnan Kumar <kkrish@google.com>
2022-09-27 21:23:06 +0530

    mimetype admin static commit

--- sipa/settings.py
+++ sipa/settings.py
@@ -135,8 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-if not DEBUG:
-        STATIC_ROOT = ''
+
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
