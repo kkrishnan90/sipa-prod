45b242df12dc58e0d230d52afbd9fb9e40f94ca0
Krishnan Kumar <kkrish@google.com>
2022-09-27 21:12:28 +0530

    mimetype admin static commit

--- sipa/settings.py
+++ sipa/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 # STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
